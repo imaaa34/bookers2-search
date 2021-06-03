0962b14e7286404ce794a256c5f0c2032740de83
imaaa34 <oct.imd34@gmail.com>
2021-06-03 13:32:41 +0000

    [Fix] 不要な記述を減らし修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,13 +3,13 @@ class SearchController < ApplicationController
   def search
     @range = params[:range]
 
-    # search = params[:search]
-    # word = params[:word]
-    # @users = User.looks(search, word)
+    search = params[:search]
+    @word = params[:word]
+
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
+      @users = User.looks(search, @word)
     else
-      @books = Book.looks(params[:search], params[:word])
+      @books = Book.looks(search, @word)
     end
   end
 
