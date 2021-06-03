8403cedeb992f6494fd90a95d67dd32c7aa85239
imaaa34 <oct.imd34@gmail.com>
2021-06-03 09:05:37 +0000

    [Start] 応用課題5

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def following
+    @users = User.find(params[:id]).following.where.not(id: current_user.id)
+  end
+
+  def followers
+    @users = User.find(params[:id]).followers.where.not(id: current_user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
